3a1862978d1bf3df4f36fbc6694b844a23247df0
talespadua <talescpadua@gmail.com>
2015-12-30 13:47:35 -0200

    Loading json file, building the graph and retrieving optimal route

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
+import networkx as nx
+import json
 
-
+#
+# class
 def calculate_cost(distance, autonomy, litre_value):
     cost = (distance/autonomy) * litre_value
 
@@ -10,6 +13,19 @@ def get_best_way(origin, destiny):
 
 app = Flask(__name__)
 
+G = nx.Graph()
+
+with open('maps.json') as json_file:
+    map = json.load(json_file)
+
+
+for points in map[0]['routes']:
+    print points
+    G.add_edge(points['origin'], points['destiny'], weight=float(points['distance']))
+
+print nx.dijkstra_path(G, 'A', 'D')
+
+
 @app.route('/get_best_route')
 def index():
     map = request.args.get('map')
@@ -18,5 +34,6 @@ def index():
     autonomy = request.args.get('autonomy')
     litre_value = request.args.get('litre_value')
     print map, origin, destiny, autonomy, litre_value
+
 
 app.run(debug=True)
